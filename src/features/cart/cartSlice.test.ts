01f16ce1c72c315a1d3f2b266128bf9c1b792127
Alexander <Alexander_alatorre1993@hotmail.com>
2022-01-11 18:45:45 -0800

    added new tests

---
+++ src/features/cart/cartSlice.test.ts
@@ -0,0 +1,7 @@
+import cartReducer, {
+  CartState,
+  updateQuantity,
+  addToCart,
+  removeFromCart,
+} from "./cartSlice";
+import type { RootState } from "../../app/store";
--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -2,12 +2,12 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import type { Product } from "../../app/api";
 
 export interface ProductsState {
-  products: { [id: string]: Product }
+  products: { [id: string]: Product };
 }
 
 const initialState: ProductsState = {
-  products: {}
-}
+  products: {},
+};
 
 const productsSlice = createSlice({
   initialState,
@@ -15,10 +15,10 @@ const productsSlice = createSlice({
   reducers: {
     receivedProducts(state, action: PayloadAction<Product[]>) {
       const products = action.payload;
-      products.forEach(product => {
+      products.forEach((product) => {
         state.products[product.id] = product;
-      })
-    }
+      });
+    },
   },
 });
 
